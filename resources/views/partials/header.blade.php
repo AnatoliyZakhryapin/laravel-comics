30589ee03b00da7772c9dd33e637d73748e3c62d
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-11 16:31:52 +0100

    ADD partials header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="logo">
+                    Logo
+                </div>
+            </div>
+            <div class="col">
+                <div class="nav-bar">
+                    Nav Bar
+                </div>
+            </div>
+            <div class="col">
+                <div class="search-input">
+                    Input
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
