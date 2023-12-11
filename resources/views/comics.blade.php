5fb20ce28c3af90f1cc36bf75b5ad5853d8993a7
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-11 17:34:50 +0100

    print 1 card to page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,5 +3,13 @@
 @section('content')
     <section>
         <h1>Comics</h1>
+
+        
+        <div class="card" style="width: 18rem;">
+            <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{ $comics[0]['title'] }}</h5>
+            </div>
+          </div>
     </section>
 @endsection
