6aa3cc7f7f004ce3f56dc37466d55ffaa8cd606e
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-11 17:10:13 +0100

    ADD pages to nav-bar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,25 +1,49 @@
 <header class="page-header">
     <div class="container">
         <div class="row">
-            <div class="col">
+            <div class="col-auto">
                 <div class="logo">
                     Logo
                 </div>
             </div>
-            <div class="col">
+            <div class="col d-flex justify-content-center">
                 <ul class="nav nav-underline">
                     <li class="nav-item">
-                      <a class="nav-link" href="#">Active</a>
+                        <a class="nav-link" href="{{ route('home') }}">home</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="{{ route('characters') }}">characters</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="{{ route('comics') }}">comics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('movies') }}">movies</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('tv') }}">tv</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('games') }}">games</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('collectibles') }}">collectibles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('videos') }}">videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('fans') }}">fans</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('news') }}">news</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('shop') }}">shop</a>
                     </li>
                 </ul>
             </div>
-            <div class="col">
+            <div class="col-auto">
                 <div class="search-input">
                     Input
                 </div>
